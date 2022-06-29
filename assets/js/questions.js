7dd8191e5941784e4c7cb02345bda5692caa2ea0
Dalian Grullon <dalian.business@aol.com>
2022-06-29 00:28:20 -0400

    created array of questions in questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+        title: "Which of the following are NOT a primitive data type?",
+        choices: ["String", "Number", "Undefined", "Array"],
+        answer: "Array"
+    },
+    {
+        title: "Which of the following is NOT used in Vanilla JavaScript?",
+        choices: ["var", ".getElementById", ".setAttribute", "text()"],
+        answer: "text()"
+    },
+    {
+        title: "A variable defined within a function has a global scope",
+        choices: ["True", "False",],
+        answer: "False"
+    },
+    {
+        title: "Calling the keyword 'this' within an object refers to the object it's in",
+        choices: ["True", "False"],
+        answer: "True"
+    },
+    {
+        title: "JavaScript is: ",
+        choices: ["Event-driven", "Low-level", "Browser-dependent", "A compiled language"],
+        answer: "Event-driven"
+    },
+    {
+        title: "JavaScript can be used for full-stack web development",
+        choices: ["True", "False"],
+        answer: "True"
+    },
+    {
+        title: "JavaScript is strongly related to Java",
+        choices: ["True", "False"],
+        answer: "False"
+    },
+    {
+        title: "JavaScript is a class-based programming language",
+        choices: ["True", "False"],
+        answer: "False"
+    },
+    {
+        title: "JavaScript is a prototype-based programming language",
+        choices: ["True", "False"],
+        answer: "True"
+    },
+    {
+        title: "Which of the following is the assignment operator",
+        choices: ["=", "==", "===", "!="],
+        answer: "="
+    },
+]
